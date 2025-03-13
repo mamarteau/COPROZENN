3c75c6e57f30c289ea1c93ae32fb87c150b44e28
Hyacinthe Aubert <hyacinthe.aubert@outlook.fr>
2025-03-13 18:03:03 +0100

    fix unread message counter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
 
   def compute_unread_messages_counter
     @unread_messages_counter = 0
+    return unless user_signed_in?
+
     current_user.chat_members.each do |cm|
       chatroom = cm.chatroom
       messages = chatroom.messages.where("created_at > ?", cm.connected_at)
