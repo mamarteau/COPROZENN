9286e6f915a18db1a2e5d93720db51cbebb731cd
Hyacinthe Aubert <hyacinthe.aubert@outlook.fr>
2025-03-18 12:18:20 +0100

    Bug messages en double, Notif index chatroom

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,4 @@
 class MessagesController < ApplicationController
-
-
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
     @message = Message.new(message_params)
@@ -9,14 +7,14 @@ class MessagesController < ApplicationController
     @message.chat_member = @chat_member
 
     if @message.save
-      respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.append(:messages, partial: "messages/message",
-            target: "messages",
-            locals: { message: @message, user: current_user })
-        end
-        format.html { redirect_to chatroom_path(@chatroom) }
-      end
+      # respond_to do |format|
+      #   format.turbo_stream do
+      #     render turbo_stream: turbo_stream.append(:messages, partial: "messages/message",
+      #       target: "messages",
+      #       locals: { message: @message, user: current_user })
+      #   end
+      #   format.html { redirect_to chatroom_path(@chatroom) }
+      # end
     else
       render "chatrooms/show", status: :unprocessable_entity
     end
@@ -27,6 +25,4 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:content)
   end
-
-
 end
