da44ca4c9d0da1e444b0b15018debf78570740db
Marie-Adeline Marteau <marieadelinemarteau@gmail.com>
2025-03-19 17:37:06 +0100

    map ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,28 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
   def home
     @meetings = Meeting.all
     @decision = Decision.new
     @title = "Gérer votre copro en toute serenité!"
+  end
+
+  def floors
+  end
+
+  def user_infos
+    user_email = User::USER_APPARTMENTS[params[:appartment_id]] if params[:appartment_id].present?
+    user = User.find_by(email: user_email) if user_email
+
+    respond_to do |format|
+      format.json do
+        render json: {
+          html: render_to_string(
+            partial: "shared/appartment_user_infos",
+            locals: { user: user },
+            formats: :html,
+            layout: false
+          )
+        }
+      end
+    end
   end
 end
---
+++ app/javascript/controllers/floor_controller.js
@@ -0,0 +1,45 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="floor"
+export default class extends Controller {
+  static targets = ["appartment", "userInfosContainer", "floorsContainer", "floorNumber"]
+
+  connect() {
+    this.currentFloor = 1;
+  }
+
+  change(event) {
+    this.appartmentTargets.forEach(t => t.setAttribute("fill", "white"))
+    event.currentTarget.setAttribute("fill", "#fe6363");
+    const appartmentId = event.currentTarget.id;
+    fetch(`/user_infos?appartment_id=${appartmentId}`, {
+      headers: {
+        'Accept': "application/json"
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      this.userInfosContainerTarget.innerHTML = data.html;
+    })
+  }
+
+  displayFloor() {
+    const firstFloor = this.floorsContainerTarget.querySelector("div");
+    firstFloor.style.marginLeft = `-${(this.currentFloor - 1) * 100}%`;
+    this.floorNumberTarget.innerText = this.currentFloor;
+  }
+
+  previousFloor(event) {
+    if (this.currentFloor === 1) return;
+
+    this.currentFloor -= 1;
+    this.displayFloor()
+  }
+
+  nextFloor(event) {
+    if (this.currentFloor === 3) return;
+
+    this.currentFloor += 1;
+    this.displayFloor();
+  }
+}
